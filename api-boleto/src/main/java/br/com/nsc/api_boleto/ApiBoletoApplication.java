7fadc2ce556cec1c1773cfe3b722b6346f5bde26
Nelson Costa <nelsonscosta@yahoo.com.br>
2026-02-16 13:13:14 -0300

    criado api-boleto

---
+++ api-boleto/src/main/java/br/com/nsc/api_boleto/ApiBoletoApplication.java
@@ -0,0 +1,13 @@
+package br.com.nsc.api_boleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBoletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBoletoApplication.class, args);
+	}
+
+}
---
+++ api-boleto/src/test/java/br/com/nsc/api_boleto/ApiBoletoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nsc.api_boleto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBoletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
